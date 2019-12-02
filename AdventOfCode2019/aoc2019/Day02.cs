6670e78ac2d7c946bbeb38771a0fb0a390a5e114
Johan Ostrand <muztanger@gmail.com>
2019-12-02 18:47:54 +0100

    Day02 part 2

--- AdventOfCode2019/aoc2019/Day02.cs
+++ AdventOfCode2019/aoc2019/Day02.cs
@@ -62,11 +62,44 @@ namespace aoc2019
         [TestMethod]
         public void Part2()
         {
-            
+            var init = input.Split(",").Select(x => int.Parse(x.Trim())).ToArray();
+            for (int noun = 0; noun < 100; noun++)
+                for (int verb = 0; verb < 100; verb++)
+                { 
+                    int instructionPointer = 0;
+                    var memory = new int[init.Length];
+                    for (int i = 0; i < init.Length; i++)
+                    {
+                        memory[i] = init[i];
+                    }
+                    memory[1] = noun; //noun
+                    memory[2] = verb; //verb
+                    //int[] memory = Enumerable.Range(0, 4096).Select(_ => 0).ToArray();
+
+                    int instruction = memory[0];
+                    while (instruction != 99)
+                    {
+                        switch (instruction)
+                        {
+                            case 1:
+                                memory[memory[instructionPointer + 3]] = memory[memory[instructionPointer + 1]] + memory[memory[instructionPointer + 2]];
+                                instructionPointer += 4;
+                                break;
+                            case 2:
+                                memory[memory[instructionPointer + 3]] = memory[memory[instructionPointer + 1]] * memory[memory[instructionPointer + 2]];
+                                instructionPointer += 4;
+                                break;
+                            case 99:
+                                instructionPointer += 1;
+                                break;
+                            default:
+                                throw new Exception($"Something went wrong opCode={instruction}");
+                        }
+                        instruction = memory[instructionPointer];
+                    }
+                    if (memory[0] == 19690720) throw new Exception($"100 * {noun} + {verb} = {100 * noun + verb}");
+                }
         }
-
-
-
 
         private static string input = @"1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0";
     }
