ad3596fb9107585de330381b5b3f59c9a56334c2
Johan Ostrand <muztanger@gmail.com>
2019-12-03 22:33:18 +0100

    Cleanup of day3 and moved common classes to a common directory

---
+++ AdventOfCode2019/aoc2019/common/Pos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace aoc2019
+{
+    public class Pos : IEquatable<Pos>
+    {
+        public int x;
+        public int y;
+        public Pos(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Pos(Pos other)
+        {
+            this.x = other.x;
+            this.y = other.y;
+        }
+
+        public static Pos operator *(Pos p1, int n)
+        {
+            return new Pos(p1.x * n, p1.y * n);
+        }
+
+        public static Pos operator +(Pos p1, Pos p2)
+        {
+            return new Pos(p1.x + p2.x, p1.y + p2.y);
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+
+        internal int manhattan(Pos inter)
+        {
+            return Math.Abs(x - inter.x) + Math.Abs(y - inter.y);
+        }
+
+        bool IEquatable<Pos>.Equals(Pos other)
+        {
+            return this.x == other.x && this.y == other.y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Pos);
+        }
+
+        public bool Equals([AllowNull] Pos other)
+        {
+            return other != null &&
+                   x == other.x &&
+                   y == other.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() * 7919 + y.GetHashCode();
+        }
+    }
+}
